08cdc12a9902b220752eede97168f9c3adb4da08
Ariel Herman <aeh.herman@gmail.com>
2017-12-04 15:41:38 -0500

    add current user route

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,30 +4,31 @@ class Api::V1::AuthController < ApplicationController
   # generate secret with rake secret
   # Hide the secret by including in your gitignore with figaro gem and replacing in code with ENV['secret']
 
-def create
-  user = User.find_by(username: params[:username])
-  if user && user.authenticate(params[:password])
-  # issue user a token
-    render json: {
-      id: user.id,
-      name: user.name,
-      username:user.username,
-      gender_choice: user.gender_choice,
-      interests: user.interests,
-      matches: user.matches,
-      location: user.location
-    }
-  else
-    render json: {error: "Could not authorize this user"}, status: 401
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+    # issue user a token
+      render json: {
+        id: user.id,
+        name: user.name,
+        username:user.username,
+        gender_choice: user.gender_choice,
+        interests: user.interests,
+        matches: user.matches,
+        location: user.location
+      }
+    else
+      render json: {error: "Could not authorize this user"}, status: 401
+    end
   end
-end
 
-def show
-  byebug
-  if logged_in?
-    # render json for current users
-  else
-    # render json error
-end
+  def show
+    byebug
+    if logged_in?
+      # render json for current users
+    else
+      # render json error
+    end
 
+  end
 end
